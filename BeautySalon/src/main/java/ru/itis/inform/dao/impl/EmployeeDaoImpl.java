3ff751580fc202cbbf54f728e2709eb5b7d4683f
Артур Мустаев < >
2016-12-12 14:39:19 +0300

    employee

--- BeautySalon/src/main/java/ru/itis/inform/dao/impl/EmployeeDaoImpl.java
+++ BeautySalon/src/main/java/ru/itis/inform/dao/impl/EmployeeDaoImpl.java
@@ -1,12 +1,18 @@
 package ru.itis.inform.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.itis.inform.dao.interfaces.EmployeeDao;
+import ru.itis.inform.dao.mappers.EmployeeMapper;
 import ru.itis.inform.models.Employee;
 import ru.itis.inform.models.Record;
 import ru.itis.inform.models.WorkTime;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by Manymuch on 19.10.2016.
@@ -14,8 +20,11 @@ import java.util.List;
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
 
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     private static String SQL_SAVE = "INSERT INTO employee (last_name, first_name, middle_name, specialization_id)" +
-            " VALUES (:lastName, :firstName, :middleName, :specializationId); ";
+            " VALUES (:lastName, :firstName, :middleName, :specializationId) RETURNING employee.employee_id;";
     private static String SQL_DELETE = "DELETE FROM employee WHERE (employee_id = :employeeId);";
     private static String SQL_UPDATE = "UPDATE employee SET (last_name, first_name, middle_name, specialization_id) " +
             "= (:lastName, :firstName, :middleName, :specializationId) WHERE (employee_id = :employeeId);";
@@ -28,41 +37,66 @@ public class EmployeeDaoImpl implements EmployeeDao {
 
     @Override
     public int saveEmployee(Employee employee) {
-        return 0;
+        Map<String, Object> params = new HashMap<>();
+        params.put("lastName", employee.getLastName());
+        params.put("firstName", employee.getFirstName());
+        params.put("middleName", employee.getMiddleName());
+        params.put("specializationId", employee.getSpecialization().getId());
+        return namedParameterJdbcTemplate.queryForObject(SQL_SAVE, params, int.class);
     }
 
     @Override
     public void deleteEmployee(int id) {
-
+        Map<String, Object> params = new HashMap<>();
+        params.put("employeeId", id);
+        namedParameterJdbcTemplate.update(SQL_DELETE, params);
     }
 
     @Override
     public void updateEmployee(Employee employee) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("employeeId", employee.getId());
+        params.put("lastName", employee.getLastName());
+        params.put("firstName", employee.getFirstName());
+        params.put("middleName", employee.getMiddleName());
+        params.put("specializationId", employee.getSpecialization().getId());
+        namedParameterJdbcTemplate.update(SQL_UPDATE, params);
 
     }
 
     @Override
     public Employee getEmployee(int id) {
-        return null;
+        Map<String, Object> params = new HashMap<>();
+        params.put("employeeId", id);
+        return (Employee) namedParameterJdbcTemplate.queryForObject(SQL_GET_BY_ID, params, new EmployeeMapper());
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        return namedParameterJdbcTemplate.query(SQL_GET_ALL, new EmployeeMapper());
     }
 
     @Override
     public List<Record> getEmployeeRecordsById(int id) {
-        return null;
+        Map<String, Object> params = new HashMap<>();
+        params.put("employeeId", id);
+        return namedParameterJdbcTemplate.query(SQL_GET_RECORDS_BY_ID, params, new EmployeeMapper());
     }
 
     @Override
     public List<WorkTime> getEmployeeWorkTime(int id) {
-        return null;
+        Map<String, Object> params = new HashMap<>();
+        params.put("employeeId", id);
+        return namedParameterJdbcTemplate.query(SQL_GET_WORK_TIME, params, new EmployeeMapper());
     }
 
     @Override
     public void updateWorkTime(int id, WorkTime workTime) {
-
+        Map<String, Object> params = new HashMap<>();
+        params.put("employeeId", id);
+        params.put("weekday", workTime.getWeekday());
+        params.put("startTime", workTime.getStartTime());
+        params.put("endTime", workTime.getEndTime());
+        namedParameterJdbcTemplate.update(SQL_UPDATE_WORK_TIME, params);
     }
 }
