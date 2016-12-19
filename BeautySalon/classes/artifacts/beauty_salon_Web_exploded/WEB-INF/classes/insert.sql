8caa5740838abb224db16193d00959aed97dbc16
morjonan <skotchya@gmail.com>
2016-12-19 16:28:07 +0300

    delete classes xD

--- BeautySalon/classes/artifacts/beauty_salon_Web_exploded/WEB-INF/classes/insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO specialization (type) VALUES ('Hair cutter');
-INSERT INTO employee (last_name, first_name, middle_name, specialization_id, phone)
-    VALUES ('Ivanov', 'Ivan', 'Ivanovich', 1, '88005553535');
-
-INSERT INTO service (type, specialization_id, price) VALUES ('Hair cut', 1, 229);
-INSERT INTO service (type, specialization_id, price) VALUES ('Hair cut x2', 1, 1337);
